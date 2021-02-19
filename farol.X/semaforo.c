4397771a3758acc4e483acf9fcf8560b5a621082
20185327 <20185327@DSN1271158622.cfp127aluno.local>
2021-02-19 15:30:19 -0200

    criação de projeto semaforo

---
+++ farol.X/semaforo.c
@@ -0,0 +1,16 @@
+/*
+ * File:   semaforo.c
+ * Author: 20185327
+ *
+ * Created on 19 de Fevereiro de 2021, 15:15
+ */
+
+
+#include <xc.h>
+#include "config.h"
+void semaforo_init(void)
+{
+
+
+
+}
